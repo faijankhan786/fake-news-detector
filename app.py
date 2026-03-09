d3d2ee29983cf038fd7907b1aeba385706cfdc2b
Md.Faijan Khan <kingfaij9@gmail.com>
2026-03-09 14:41:37 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib
+import json
+
+
+if st.query_params.get("endpoint") == "health":
+    st.write(json.dumps({"status": "ok"}))
+    st.stop()
+
+model = joblib.load('model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+st.title("📰 Fake News Detector")
+text = st.text_area("Enter news article:")
+
+if st.button("Predict"):
+    if text.strip() == "":
+        st.warning("Please enter some news text")
+    else:
+        vec = vectorizer.transform([text])
+        result = model.predict(vec)[0]
+        if result == 1:
+            st.error("🔴 Fake News Detected!")
+        else:
+            st.success("🟢 This is Real News!")
